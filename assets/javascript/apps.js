5efa956d55f0218cc7b4671e1c7dfcdd20b88d07
Karen Astell <KarenAstell@gmail.com>
2020-04-11 09:55:56 -0400

    file structure created

---
+++ assets/javascript/apps.js
@@ -0,0 +1,12 @@
+// var wantNumbers = confirm("Do you want it to contain numbers?");
+// console.log(wantNumbers);
+
+// var upperCase = confirm("Do you want it to contain upper case letters?");
+// console.log(upperCase);
+
+// var lowerCase = confirm("Do you want it to contain lower case letters?");
+// console.log(lowerCase);
+
+// var specialCharacters = confirm("Do you want it to contain special characters?");
+// console.log(specialCharacters);
+
