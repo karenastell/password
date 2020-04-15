ce80e65368764eb1fe1707caead85f8a6405419a
Karen Astell <KarenAstell@gmail.com>
2020-04-15 09:25:26 -0400

    added the object with all possible password characters

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,3 +1,11 @@
+
+var password = {
+    wantNumbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    upperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    lowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    specialCharacters: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", ">", "<", ".", ","]
+}
+
 // var wantNumbers = confirm("Do you want it to contain numbers?");
 // console.log(wantNumbers);
 
@@ -9,4 +17,6 @@
 
 // var specialCharacters = confirm("Do you want it to contain special characters?");
 // console.log(specialCharacters);
+
+
 
