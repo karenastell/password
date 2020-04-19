411f1361160de5956e8f97a40a9929291461a3d8
Karen Astell <KarenAstell@gmail.com>
2020-04-18 22:15:43 -0400

    concatenate arrays depending on user choice of characters

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,22 +1,71 @@
 
-var password = {
-    wantNumbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    upperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-    lowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-    specialCharacters: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", ">", "<", ".", ","]
+var pwLength = prompt("How long would you like your password? (between 8-128 characters)");
+// check password length is within parameters 
+while (pwLength < 8 || pwLength > 128) {
+    length = prompt("Your password can be between 8-128 characters, please select password length");
+}
+console.log(pwLength);
+
+var wantNumbers = confirm("Do you want it to contain numbers?");
+console.log(wantNumbers);
+
+var wantUpperCase = confirm("Do you want it to contain upper case letters?");
+console.log(wantUpperCase);
+
+var wantLowerCase = confirm("Do you want it to contain lower case letters?");
+console.log(wantLowerCase);
+
+var wantSpecialCharacters = confirm("Do you want it to contain special characters?");
+console.log(wantSpecialCharacters);
+
+
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", ">", "<", ".", ","];
+
+
+if (wantNumbers === false) {
+    numbers = [];
+};
+if (wantUpperCase === false) {
+    upperCase = [];
+};
+if (wantLowerCase === false) {
+    lowerCase = [];
+}
+if (wantSpecialCharacters === false) {
+    specialCharacters = [];
 }
 
-// var wantNumbers = confirm("Do you want it to contain numbers?");
-// console.log(wantNumbers);
 
-// var upperCase = confirm("Do you want it to contain upper case letters?");
-// console.log(upperCase);
 
-// var lowerCase = confirm("Do you want it to contain lower case letters?");
-// console.log(lowerCase);
+var password = numbers.concat(upperCase, lowerCase, specialCharacters);
+console.log(password);
+console.log(password.length);
 
-// var specialCharacters = confirm("Do you want it to contain special characters?");
-// console.log(specialCharacters);
+// for (var i = 0; i === password.length; i++) {
+
+// ramdonly chooses a number between 0-3
+var randomIndex = Math.floor(Math.random() * password.length);
+console.log(randomIndex);
+// takes the random number and assigns it to Rock, Paper or Scissors
+var pass = password[randomIndex];
+console.log(pass);
+
+
+    // var randNum = Math.floor(randNum);
+    // var randNum = Math.floor(Math.random() * password.length);
+    // console.log(randNum);
+    // console.log(password[randNum]);
+    //var newPassword = password[randNum];
+    //console.log(newPassword);
+// };
+//     // round down to the nearest integer
+
+//   
+// }
+
 
 
 
